a6c5d670c84916fe51139d0ea070f72b7afc9a9a
DeepaRaj123 <deeparaj096@gmaile.com>
2025-01-25 16:25:52 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -57,5 +57,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Export the app for Vercel
-module.exports = app;
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
